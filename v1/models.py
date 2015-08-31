ca490cb6497cb25e3b0f872a32e0ca1c2b37b87c
Camron Godbout <camron248@gmail.com>
2015-08-30 23:07:31 -0400

    pep8 compliance

--- v1/models.py
+++ v1/models.py
@@ -11,4 +11,5 @@ class Post(models.Model):
 class User(AbstractBaseUser):
     profile_picture = models.ImageField(upload_to='', blank=True)
     password_token = models.CharField(max_length=40, null=True, default='')
-    authentication_token = models.CharField(max_length=40, null=True, default='')
+    authentication_token = models.CharField(max_length=40, null=True,
+                                            default='')
