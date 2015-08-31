7a0feef89291df966898abc6aad42d4bf581d5ab
Trevor Hutto <huttotw@gmail.com>
2015-08-31 10:39:01 -0400

    /users/self/

--- v1/views.py
+++ v1/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 from v1.serializers import UserSerializer
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 
@@ -10,3 +12,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [TokenHasReadWriteScope]
+
+    @list_route(url_path='self')
+    def self(self, request):
+    	serializer = self.get_serializer(request.user)
+    	return Response(serializer.data)
