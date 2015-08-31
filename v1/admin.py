549745cc2300b0972a280256c62e039f2e6e024f
Trevor Hutto <huttotw@gmail.com>
2015-08-30 21:55:25 -0400

    working build

--- v1/admin.py
+++ v1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, UserProfile
+from .models import Post, User
 
 admin.site.register(Post)
-admin.site.register(UserProfile)
+admin.site.register(User)
