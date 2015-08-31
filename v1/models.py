3c931ec0845daeb5a88fbb5b783de76e171d7bae
Camron Godbout <camron248@gmail.com>
2015-08-30 21:24:34 -0400

    models update

--- v1/models.py
+++ v1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# model for the post
+class Post(models.Models):
+    template = models.IntegerField(null=True, default=0)
+    user = models.ForeignKey(User)
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    profile_picture = models.ImageField(upload_to='', blank=True)
+    password_token = models.Charfield(length=40, null=True, default='')
+    authentication_token = models.CharField(lenght=40, null=True, default='')
