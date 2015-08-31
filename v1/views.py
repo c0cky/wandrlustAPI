a2e561b3b5a012222aa38c1eb5ea5d8cb7c4abbc
Trevor Hutto <huttotw@gmail.com>
2015-08-31 10:00:09 -0400

    added scopes check

--- v1/views.py
+++ v1/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from v1.serializers import UserSerializer
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -8,3 +9,4 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = [TokenHasReadWriteScope]
